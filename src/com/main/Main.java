1d5b4b01fc07f10f6d802ba9e60bfb8fc895abca
Dev Manek <manekdev2001@gmail.com>
2021-04-25 13:32:19 +0530

    Added Files

---
+++ src/com/main/Main.java
@@ -0,0 +1,10 @@
+package com.main;
+
+import com.project.GUI.GUIMethods;
+
+public class Main {
+    public static void main(String[] args) {
+        GUIMethods GUIMethods = new GUIMethods();
+        GUIMethods.Login();
+    }
+}
